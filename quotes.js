d168454c65b89c9ff16d8be77ce079c29732ce5a
waelzam <zamwael@gmail.com>
2021-09-19 23:50:20 +0100

    first commit

---
+++ quotes.js
@@ -0,0 +1,30 @@
+const localQuotes = [
+    {
+      text: 'Rojla s3iba.',
+      author: 'ZAM',
+    },
+    {
+      text: 'yambaddel le7ya b le7ya techteglhom lethnin.',
+      author: 'ZAM',
+    },
+    {
+      text: 'Rojla matetchrech b flous.',
+      author: 'ZAM',
+    },
+    {
+      text: 'wa9t chedda tal9a ken zammoula.',
+      author: 'ZAM',
+    },
+    {
+      text: '25 septembre ;)',
+      author: 'ZAM',
+    },
+    {
+      text: 'careful boy i enjoy you.',
+      author: 'ZAM',
+    },
+    {
+      text: 'maydoum f wed ken 7ajrou.',
+      author: 'ZAM',
+    }
+]
--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const quoteBtn = document.getElementById('new-quote');
+//let apiQuotes =[];
+//Show newQuote 
+function newQuote(){
+   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+   //check if field is blanc and replace it
+   if (!quote.author)
+   {
+    authorText.textContent = 'no author';
+   }
+   else{
+    authorText.textContent =quote.author;
+   }
+//    check quote length 
+if (quote.text.length > 50 )
+{
+    quoteText.classList.add('long-quote');
+}
+else{
+    quoteText.classList.remove('long-quote');
+}
+    quoteText.textContent = quote.text;
+ 
+   
+}
+// tweet button
+function tweetQuote()
+{
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank')
+}
+// Getting quotes from API
+/* async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        
+        // handle error here
+    }
+} */
+//Event Listeners
+quoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+newQuote();
+// when page starts 
+//getQuotes();
